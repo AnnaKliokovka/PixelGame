8e61d46fa09beef1591dad3d2f1dbc8bd14ab547
Anna Gribtsova <adgribtsova@mai.education>
2022-09-08 22:15:10 +0300

    Движение камеры
    Теперь камера может перемещаться только по одной оси, которую можно настроить в инспекторе

--- Assets/Scripts/CameraMotor.cs
+++ Assets/Scripts/CameraMotor.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraMotor : MonoBehaviour
 {
+    public bool horizontal = true;
+    public bool vertical = true;
+
     public Transform lookAt;
     public float boundX = 0.15f;
     public float boundY = 0.05f;
@@ -12,27 +15,33 @@ public class CameraMotor : MonoBehaviour
     {
         Vector3 delta = Vector3.zero;
         float deltaX = lookAt.position.x - transform.position.x;
-        if (deltaX > boundX || deltaX < -boundX)
+        if (horizontal)
         {
-            if (transform.position.x < lookAt.position.x)
+            if (deltaX > boundX || deltaX < -boundX)
             {
-                delta.x = deltaX - boundX;
-            }
-            else
-            {
-                delta.x = deltaX + boundX;
+                if (transform.position.x < lookAt.position.x)
+                {
+                    delta.x = deltaX - boundX;
+                }
+                else
+                {
+                    delta.x = deltaX + boundX;
+                }
             }
         }
         float deltaY = lookAt.position.y - transform.position.y;
-        if (deltaY > boundY || deltaY < -boundY)
+        if (vertical)
         {
-            if (transform.position.y < lookAt.position.y)
+            if (deltaY > boundY || deltaY < -boundY)
             {
-                delta.y = deltaY - boundY;
-            }
-            else
-            {
-                delta.y = deltaY + boundY;
+                if (transform.position.y < lookAt.position.y)
+                {
+                    delta.y = deltaY - boundY;
+                }
+                else
+                {
+                    delta.y = deltaY + boundY;
+                }
             }
         }
         transform.position += new Vector3(delta.x, delta.y, 0);
